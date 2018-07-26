898bf25b63bd26ee6e88d6d2b035e64d848e8fc9
Divya123-qisit <41731104+Divya123-qisit@users.noreply.github.com>
2018-07-26 19:29:05 +0530

    Create Postvi.java

---
+++ Postvi.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*
+import java.util.Scanner;
+class Postvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter n");
+		n=sc.nextInt();
+		if(n==0)
+		{
+			System.out.println("postive or negative");
+		}
+		else if(n>0)
+		{
+			System.out.println("postive");
+		}
+		else if(n<0)
+		{
+			System.out.println("negative");
+		}
+		
+		
+		}
+	}
