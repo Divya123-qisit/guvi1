bf1d3bbeafd9fce86fb97a9e6c5aafd90cc05fb8
Divya123-qisit <41731104+Divya123-qisit@users.noreply.github.com>
2018-07-26 16:37:23 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class Even
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int n;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter a  number");
+		n=sc.nextInt();
+		if(n%2==0)
+		{
+			System.out.println("even number");
+		}
+	else 
+	{
+		System.out.println("odd number");
+	}
+}
+	}
