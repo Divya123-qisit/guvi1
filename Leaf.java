0d7125025ec572c3629a4ec7fe81ce58d6b45348
Divya123-qisit <41731104+Divya123-qisit@users.noreply.github.com>
2018-07-27 11:08:40 +0530

    Create Leaf.java

---
+++ Leaf.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class Leaf
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int year;
+		boolean flag=false;
+		Scanner sc=new Scanner(System.in);
+		year=sc.nextInt();
+		if(year%4==0)
+		{
+			if(year%100==0)
+			{
+				if(year%400==0)
+				{
+					flag=true;
+				}
+				else
+				flag=false;
+			}
+			else
+			flag=true;
+		}
+		else 
+		flag=false;
+		if(flag==true)
+		{
+		System.out.println("Yes");
+		}
+		else
+		{
+		System.out.println("No");
+		}
+			
+		
+	}
+		
+		
+		
+			
+}
