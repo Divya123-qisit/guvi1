87b2480a08f59707a70503937d28bd068d81dfc7
Divya123-qisit <41731104+Divya123-qisit@users.noreply.github.com>
2018-07-27 11:21:54 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class Hello
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		for( int i=0;i<n;i++)
+		{
+			System.out.println("Hello");
+		
+		}
+	}
+}
