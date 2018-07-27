1e4a902cd0b500c9e4184d38361f9f5a8449eebd
Divya123-qisit <41731104+Divya123-qisit@users.noreply.github.com>
2018-07-27 15:22:14 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Prime
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int sum=0,r;
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int temp=n;
+		while(n>0)
+		{
+			r=n%10;
+			sum=(sum*10)+r;
+			n=n/10;
+		}
+		if(temp==sum)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+		
+	}
+}
