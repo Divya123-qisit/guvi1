082f6486a0e04aa98ad6d07368a22a8b7379d565
Divya123-qisit <41731104+Divya123-qisit@users.noreply.github.com>
2018-07-27 09:50:00 +0530

    Update Even.java

--- Even.java
+++ Even.java
@@ -8,15 +8,14 @@ class Even
 	{
 	  int n;
 		Scanner sc=new Scanner(System.in);
-		System.out.println("enter a  number");
 		n=sc.nextInt();
 		if(n%2==0)
 		{
-			System.out.println("even number");
+			System.out.println("Even");
 		}
 	else 
 	{
-		System.out.println("odd number");
+		System.out.println("Odd");
 	}
 }
 	}
