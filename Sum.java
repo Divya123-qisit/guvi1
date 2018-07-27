0731285012701d32e6391a630c294dc966ee105a
Divya123-qisit <41731104+Divya123-qisit@users.noreply.github.com>
2018-07-27 11:40:12 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Sum
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,sum=0;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		for(int i=0;i<=n;i++)
+		{
+		sum=sum+i;
+			}
+		System.out.println(sum);
+	}
+}
