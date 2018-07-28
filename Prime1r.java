01662c830a5cf2bdf20e7eb848b501b5452c558c
Divya123-qisit <41731104+Divya123-qisit@users.noreply.github.com>
2018-07-28 13:08:12 +0530

    Rename PrimeNumber.java to Prime1r.java

---
+++ Prime1r.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class PrimeNumber
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		for(int i=a+1;i<b;i++)
+		{
+			int counter=0;
+			for(int num=i;num>=1;num--)
+			{
+				if(i%num==0)
+				{
+					counter=counter+1;
+				}
+			}
+			if(counter==2)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+}
+		
