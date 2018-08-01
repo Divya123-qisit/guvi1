0359f3a9f24f0816ebe22f6be3a417563daec5fd
Divya123-qisit <41731104+Divya123-qisit@users.noreply.github.com>
2018-08-01 19:23:06 +0530

    Create Str.java

---
+++ Str.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Str
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		boolean numeric=true;
+		try{
+			Double num = Double.parseDouble(str);
+        } catch (NumberFormatException e) {
+            numeric = false;
+        }
+
+        if(numeric)
+            System.out.println("yes");
+        else
+            System.out.println("no");
+    }
+}
