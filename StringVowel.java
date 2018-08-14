8cb28909782bf82fcd393b6a5185cb409bfa75d0
Divya123-qisit <41731104+Divya123-qisit@users.noreply.github.com>
2018-08-14 12:59:08 +0530

    Create StringVowel.java

---
+++ StringVowel.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class StringVowel
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		int count=0;
+		for(int i=0;i<s.length();i++)
+		{
+			if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u'||s.charAt(i)=='A'||s.charAt(i)=='E'||s.charAt(i)=='I'||s.charAt(i)=='O'||s.charAt(i)=='U')
+			count++;
+		}
+		if(count>0)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}
