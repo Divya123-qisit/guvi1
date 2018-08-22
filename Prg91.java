86310e2ca0368f9664fc32091f5f787418376add
Divya123-qisit <41731104+Divya123-qisit@users.noreply.github.com>
2018-08-22 21:21:14 +0530

    Create Prg91.java

---
+++ Prg91.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Prg91
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int l=sc.nextInt();
+		int b=sc.nextInt();
+		int h=sc.nextInt();
+		int s=2*((l*b)+(b*h)+(h*l));
+		int v=l*b*h;
+		System.out.print(s +" ");
+		System.out.print(v);
+	}
+}
