04eb597ab318a6723ede4dd8131d6fe3678e762c
Divya123-qisit <41731104+Divya123-qisit@users.noreply.github.com>
2018-08-28 21:33:42 +0530

    Rename Palyer27.java to PalyerSet27.java

---
+++ PalyerSet27.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Palyer27
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner in=new Scanner(System.in);
+	String s=in.next();
+	char c[]=s.toCharArray();
+	String res="";
+	for(int i=0;i<s.length();i++){
+	if(c[i]>='a' && c[i]<='z'){
+		res=res+Character.toString(c[i]).toUpperCase();
+	}
+	else if(c[i]>='A' && c[i]<='Z'){
+		res=res+Character.toString(c[i]).toLowerCase();
+	}
+	}
+	System.out.println(res);
+	}
+}
