8a39f3a2c42c6f086f470b3ceacf270f14803359
Divya123-qisit <41731104+Divya123-qisit@users.noreply.github.com>
2018-08-28 20:51:11 +0530

    Create Player22.java

---
+++ Player22.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Player22
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int gcd=1;
+	Scanner sc=new Scanner(System.in);
+	int n1=sc.nextInt();
+	int n2=sc.nextInt();
+	for(int i = 1; i <= n1 && i <= n2; ++i)
+	{
+		if(n1 % i==0 && n2 % i==0)
+                gcd = i;
+	}
+	System.out.println(gcd);
+	}
+}
