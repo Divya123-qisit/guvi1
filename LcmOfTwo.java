ad3308d9cb284e51ef5a424a0aa4bf06bc9ef1a2
Divya123-qisit <41731104+Divya123-qisit@users.noreply.github.com>
2018-08-28 15:33:59 +0530

    Create LcmOfTwo.java

---
+++ LcmOfTwo.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class LcmOfTwo
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+		int lcm=(n1>n2) ? n1 : n2;
+		while(true)
+		{
+			if(lcm % n1 == 0 && lcm % n2 == 0)
+			{
+				System.out.println(lcm);
+				break;
+			}
+			++lcm;
+		}
+	}
+}
