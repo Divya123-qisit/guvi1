817a1b4f273f9791ca038ae826eda86af1c329fa
Divya123-qisit <41731104+Divya123-qisit@users.noreply.github.com>
2018-09-12 22:07:53 +0530

    Create Ideone4.java

---
+++ Ideone4.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone4
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine(); 
+		StringBuffer sb=new StringBuffer(str);
+		sb.append(" Answer");
+		System.out.println(sb);
+		
+	}
+}
