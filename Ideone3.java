fa07d104678a78f10de3c31643d06c3032432bae
Divya123-qisit <41731104+Divya123-qisit@users.noreply.github.com>
2018-09-14 15:47:45 +0530

    Update Ideone3.java

--- Ideone3.java
+++ Ideone3.java
@@ -6,16 +6,20 @@ class Ideone3
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-		Scanner sc=new Scanner(System.in);
+				Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
 		int k=sc.nextInt();
 		int a[]=new int[n];
+		int max=n-k;
 		for(int i=0;i<n;i++)
 		a[i]=sc.nextInt();
-		for(int i=0;i<n-k;i++)
+		for(int i=0;i<max;i++)
 		{
-			System.out.print(a[i]);
-			System.out.print(" ");
+			if(i!=max-1)
+			System.out.print(a[i]+" ");
+			else 
+				System.out.print(a[i]);
+		
 		}
 	}
 }
