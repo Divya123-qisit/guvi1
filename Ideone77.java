23abb9d8a8e13352f0bf70664e7db17c97d2818d
Divya123-qisit <41731104+Divya123-qisit@users.noreply.github.com>
2018-09-25 14:46:59 +0530

    Create Ideone77.java

---
+++ Ideone77.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone77
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		int res=~n;
+		System.out.println(res);
+		
+	}
+}
