eb0306dec336d56c0ac8cfca611910e2d49969e6
Divya123-qisit <41731104+Divya123-qisit@users.noreply.github.com>
2018-09-25 15:19:23 +0530

    Create Ideone88.java

---
+++ Ideone88.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone88
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String n=sc.next();
+		System.out.println(Integer.parseInt(n,2));
+		
+	}
+}
