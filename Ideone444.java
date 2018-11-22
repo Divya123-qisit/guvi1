02a73a792d73f7a42fc96d9fd2d84d09d12837bf
Divya123-qisit <41731104+Divya123-qisit@users.noreply.github.com>
2018-11-22 13:48:20 +0530

    Update Ideone444.java

--- Ideone444.java
+++ Ideone444.java
@@ -12,34 +12,12 @@ class Ideone444
 		// your code goes here
 	
 		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		int sum1=0,sum2=0,k=1;
-		int a[]=new int[n];
-		int b[]=new int[n];
-		for(int i=0;i<n;i++)
-		{
-			a[i]=sc.nextInt();
-			
-		}
-		for(int i=0;i<n;i++)
-		{
-			sum1=sum1+a[i];
-			
-		}
-		b[0]=sum1;
-		sum2=sum1;
-		for(int i=n-2;i>=0;i--)
-		{
-			sum2=sum2-a[i];
-			b[k]=sum2;
-			k++;
-			
-		}
-		for(int i=0;i<k;i++)
-		{
-			System.out.print(b[i] + " ");
-			
-		}
+		long a=sc.nextInt();
+		long b=sc.nextInt();
+		long c=sc.nextInt();
+		long d=(long)Math.pow(a,b);
+		long e=(long)d%c;
+		System.out.println(e);
 		
 	}
 }
