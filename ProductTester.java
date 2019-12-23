d562efbc934515dec0364fe031f1d1249628d107
Divya123-qisit <41731104+Divya123-qisit@users.noreply.github.com>
2019-12-23 23:59:35 +0530

    Create ProductTester.java

---
+++ ProductTester.java
@@ -0,0 +1,12 @@
+package com.product;
+
+public class ProductTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ProductApp app=new ProductApp("Demo.txt");
+		app.printAllBooks();
+
+	}
+
+}
